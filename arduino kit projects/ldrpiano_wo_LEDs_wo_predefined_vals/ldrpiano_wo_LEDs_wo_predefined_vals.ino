5057d80845fe512a9fc944d75922b618c82e100b
umarshehzad12 <umarshehzad@hotmail.com>
2016-06-03 10:44:57 +0500

    ldr piano code simplified even more
    Instead of using predefined values for buzzer sounds, this code uses the
    values that is getting from the LDR directly without any mapping
    function

---
+++ arduino kit projects/ldrpiano_wo_LEDs_wo_predefined_vals/ldrpiano_wo_LEDs_wo_predefined_vals.ino
@@ -0,0 +1,24 @@
+const int buzzerPin= 9;
+const int ldrPin=A0;
+int ldrValue=0;
+
+
+void setup() {
+  // put your setup code here, to run once:
+for (int i=0; i<15; i++)
+{
+  tone(buzzerPin,frequency*100, 500);
+  Serial.begin(9600);
+  delay(500);
+  }
+}
+
+void loop() {
+   ldrValue=analogRead(ldrPin);
+   Serial.println(ldrValue);
+   frequency=ldrValue;
+   tone(buzzerPin,frequency, 500);
+   delay(500);
+  // put your main code here, to run repeatedly:
+}
+
